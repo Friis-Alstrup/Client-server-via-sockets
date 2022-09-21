01def91c8cd51402c39c71e55ce26799afb1be90
Phillip Friis-Alstrup <phillip.friis.alstrup@gmail.com>
2022-09-21 10:45:57 +0200

    Add project files.

---
+++ Client/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+// Client
+
+ExecuteClient();
+
+static void ExecuteClient()
+{
+    try
+    {
+        IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName());
+        IPAddress ipAddr = ipHost.AddressList[0];
+        IPEndPoint localEndPoint = new IPEndPoint(ipAddr, 11111);
+
+        Socket sender = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+        try
+        {
+            sender.Connect(localEndPoint);
+
+            Console.WriteLine("Socket forbundet til -> {0} ", sender.RemoteEndPoint.ToString());
+
+            byte[] messageSent = Encoding.ASCII.GetBytes("Test Client<EOF>");
+            int byteSent = sender.Send(messageSent);
+
+            byte[] messageReceived = new byte[1024];
+
+            int byteRecv = sender.Receive(messageReceived);
+            Console.WriteLine("Besked fra Server -> {0}", Encoding.ASCII.GetString(messageReceived, 0, byteRecv));
+
+            sender.Shutdown(SocketShutdown.Both);
+            sender.Close();
+        }
+
+        catch (ArgumentNullException ane)
+        {
+            Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
+        }
+
+        catch (SocketException se)
+        {
+            Console.WriteLine("SocketException : {0}", se.ToString());
+        }
+
+        catch (Exception e)
+        {
+            Console.WriteLine("Unexpected exception : {0}", e.ToString());
+        }
+    }
+
+    catch (Exception e)
+    {
+        Console.WriteLine(e.ToString());
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+// Server
+
+ExecuteServer();
+
+static void ExecuteServer()
+{
+    IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName());
+    IPAddress ipAddr = ipHost.AddressList[0];
+    IPEndPoint localEndPoint = new IPEndPoint(ipAddr, 11111);
+
+    Socket listener = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+    try
+    {
+        listener.Bind(localEndPoint);
+        listener.Listen(10);
+
+        while (true)
+        {
+            Console.WriteLine("Venter på forbindelse ... ");
+
+            Socket clientSocket = listener.Accept();
+
+            byte[] bytes = new Byte[1024];
+            string data = null;
+
+            while (true)
+            {
+                int numByte = clientSocket.Receive(bytes);
+
+                data += Encoding.ASCII.GetString(bytes, 0, numByte);
+                if (data.IndexOf("<EOF>") > -1)
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine("Besked modtaget -> {0} ", data);
+            byte[] message = Encoding.ASCII.GetBytes("Test Server");
+
+            clientSocket.Send(message);
+
+            clientSocket.Shutdown(SocketShutdown.Both);
+            clientSocket.Close();
+        }
+    }
+
+    catch (Exception e)
+    {
+        Console.WriteLine(e.ToString());
+    }
+}
